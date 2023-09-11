7004dd40edb360bd6aff9172e231e7d48e117149
Lautaro Cherro <lautaro.cherro@mercadolibre.com>
2023-09-11 11:08:14 -0300

    Update run.py

--- run.py
+++ run.py
@@ -79,7 +79,7 @@ class IPCMeli:
             month_message = "se mantiene en"
 
         # Get tweet content
-        self.tweet_content += f'🇦🇷 La inflación según Mercado Libre del {get_today_str()}\n\n'
+        self.tweet_content += f'🇦🇷 Inflación según Mercado Libre del {get_today_str()}\n\n'
         self.tweet_content += f'{emoji} Se registró una inflación del {self.today_inflation}%\n'
             
         # Check wheter it's the last day of month
