ea94e2bde67ca1f996ea3d9a3b8e1ad0ce4d5330
Lautaro Cherro <cherrolautaro@gmail.com>
2024-03-28 22:18:21 -0300

    Actualizar run.py
    Actualizar dataset_handling.py
    
    Actualizar dataset_handling.py
    
    Actualizar requirements.txt

--- dataset_handling.py
+++ dataset_handling.py
@@ -4,7 +4,6 @@ import pandas as pd
 import requests
 import os
 
-from dotenv import load_dotenv
 
 from util import get_now_arg
 
@@ -66,7 +65,6 @@ def get_items_prices(items: List[str]) -> Dict[str, float]:
     :param items: a list containing the id's of the items
     :return: a dictionary containing the id's of the items as keys and their prices as values
     """
-    load_dotenv()
     access_token = os.environ.get("MELI_ACCESS_TOKEN")
 
     # Split the items dict into chunks of 20
